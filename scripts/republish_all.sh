915450476633f65209d8ff521264ad73446340f0
kenanbit <kenan.bitikofer@joinroot.com>
2021-01-11 12:19:25 -0500

    Add tag inclusion to song publishing, add republish all

--- scripts/republish_all.sh
+++ scripts/republish_all.sh
@@ -10,16 +10,16 @@ $SCRIPT_DIR/generate_all_outputs.sh
 $SCRIPT_DIR/generate_all_hymn_pages.sh
 
 # Commit the lilypond file and midi output and updated pages
-git add "lilypond/songs/$BASE_NAME"
-git add docs/local_lilypond_outputs/$BASE_NAME*
-git add docs/listing/$BASE_NAME*
+git add "lilypond/songs/"
+git add docs/local_lilypond_outputs/
+git add docs/listing/
 git add docs/hymn-index*
 git add docs/tags
 
 # Commit and push
-git commit -m "Add new song \"$BASE_NAME\""
+git commit -m "Republish all songs"
 git push
 
 # Add the song outputs to the release
 LATEST_RELEASE=`gh release list | head -n1 | awk '{print $1;}'`
-gh release upload --clobber $LATEST_RELEASE "docs/local_lilypond_outputs/$BASE_NAME"*
+gh release upload --clobber $LATEST_RELEASE "docs/local_lilypond_outputs/"*
