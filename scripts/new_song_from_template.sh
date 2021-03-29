72679b2f90b0987f0320214ab9ee840255446ccb
Kenan Schaefkofer <kbitikofer@gmail.com>
2021-03-29 17:36:47 -0400

    Make new_song_from_template more portable

--- scripts/new_song_from_template.sh
+++ scripts/new_song_from_template.sh
@@ -2,7 +2,7 @@
 
 INPUT="lilypond/hymn_template/hymn_template.ly"
 NEW_BASE=$1
+mkdir -p "lilypond/songs/$NEW_BASE"
 OUTPUT="lilypond/songs/$NEW_BASE/$NEW_BASE.ly"
 DATE=$(date '+%Y-%m-%d')
-mkdir "lilypond/songs/$NEW_BASE"
-cp $INPUT $OUTPUT && sed -i '' 's/%YYYY-MM-DD%/"'$DATE'"/g' $OUTPUT
+cat $INPUT | sed 's/%YYYY-MM-DD%/"'$DATE'"/g' > $OUTPUT
