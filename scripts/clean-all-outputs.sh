a2db959bb01b257168695c597952ae810468548e
Kenan Schaefkofer <kbitikofer@gmail.com>
2021-04-25 14:17:56 -0400

    Update with new extensions

--- scripts/clean-all-outputs.sh
+++ scripts/clean-all-outputs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo "Are you sure you would like to delete all *.pdf, *.midi, *.png recursively"
+echo "Are you sure you would like to delete all *.pdf, *.midi, *.png, *.mp3, *.odp recursively"
 echo "in the current directory `pwd` ?"
 read -r -p "[y/N] " response
 case "$response" in
-    [yY][eE][sS]|[yY]) 
+    [yY][eE][sS]|[yY])
         echo "Clearing out files..."
         find . -type f -name '*.pdf' -exec rm {} +
         find . -type f -name '*.midi' -exec rm {} +
         find . -type f -name '*.png' -exec rm {} +
+        find . -type f -name '*.mp3' -exec rm {} +
+        find . -type f -name '*.odp' -exec rm {} +
         echo "Done."
         ;;
     *)
