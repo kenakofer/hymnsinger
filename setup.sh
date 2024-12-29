7482453cfe97b676526d0963c14f5546f30a1ea8
Kenan Schaefkofer <kbitikofer@gmail.com>
2024-12-28 21:01:35 -0800

    Fix missing apache fields

--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
-jekyll new .
+# dev.sh
+docker build --build-arg ENVIRONMENT=dev -t jekyll-site . && \
+docker run -p 8080:80 jekyll-site
+
+docker run -it --entrypoint /bin/sh -p 8080:80 jekyll-site
+
+# prod.sh
+docker build --build-arg ENVIRONMENT=prod -t jekyll-site . && \
+docker run -p 80:80 -p 443:443 \
+    -e ENVIRONMENT=prod \
+    -e PASSWORD="your_secure_password" \
+    -e EMAIL="your@email.com" \
+    -v letsencrypt:/etc/letsencrypt \
+    jekyll-site
